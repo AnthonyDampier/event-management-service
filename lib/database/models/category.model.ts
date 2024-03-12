42a36b2b7153a8e7794b19025dbc8010e6caa4d2
AnthonyDampier <Dampieranthony2@gmail.com>
2024-03-12 10:44:21 -0500

    Refactor database connection and import statements

--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -1,16 +1,13 @@
-import { Schema, models, model } from 'mongoose';
+import { Document, Schema, model, models } from "mongoose";
 
 export interface ICategory extends Document {
     _id: string;
     name: string;
-    description?: string;
-    created_at: Date;
-    updated_at: Date;
 }
 
 const CategorySchema = new Schema({
-    name: { type: String, required: true, unique: true},
-});
+    name: { type: String, required: true, unique: true },
+})
 
 const Category = models.Category || model('Category', CategorySchema);
 
--- lib/database/models/event.model.ts
+++ lib/database/models/event.model.ts
@@ -1,39 +1,35 @@
-import { Document, Schema, model, models } from 'mongoose';
+import { Document, Schema, model, models } from "mongoose";
 
 export interface IEvent extends Document {
     _id: string;
     title: string;
-    description: string;
-    date: Date;
+    description?: string;
     location?: string;
-    imageURL: string;
-    created_at: Date;
-    updated_at: Date;
+    createdAt: Date;
+    imageUrl: string;
     startDateTime: Date;
     endDateTime: Date;
-    price?: string;
+    price: string;
     isFree: boolean;
     url?: string;
-    category: { _id: string, name: string};
-    organizer: { _id: string, firstName: string, lastName: string};
+    category: { _id: string, name: string }
+    organizer: { _id: string, firstName: string, lastName: string }
 }
 
 const EventSchema = new Schema({
     title: { type: String, required: true },
-    description: { type: String},
-    date: { type: Date, required: true },
-    location: { type: String,},
-    imageURL: { type: String, required: true },
-    created_at: { type: Date, default: Date.now },
-    updated_at: { type: Date, default: Date.now },
+    description: { type: String },
+    location: { type: String },
+    createdAt: { type: Date, default: Date.now },
+    imageUrl: { type: String, required: true },
     startDateTime: { type: Date, default: Date.now },
     endDateTime: { type: Date, default: Date.now },
-    price: { type: String,},
-    isFree: { type: Boolean, required: false },
-    url: { type: String, required: false },
-    category: { type: Schema.Types.ObjectId, ref: 'Category'},
-    organizer: { type: Schema.Types.ObjectId, ref: 'User'},
-});
+    price: { type: String },
+    isFree: { type: Boolean, default: false },
+    url: { type: String },
+    category: { type: Schema.Types.ObjectId, ref: 'Category' },
+    organizer: { type: Schema.Types.ObjectId, ref: 'User' },
+})
 
 const Event = models.Event || model('Event', EventSchema);
 
